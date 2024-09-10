077417c954fa7cfe9335b8c8f93f615fc5261cf0
YaelB-C <yburand2003@gmail.com>
2024-09-10 11:04:01 +0200

    exo 10sept

--- 15-Structures_de_controle/15_4-while_ET_do_while/P15_4_a-Calcul_interets/src/main/kotlin/main.kt
+++ 15-Structures_de_controle/15_4-while_ET_do_while/P15_4_a-Calcul_interets/src/main/kotlin/main.kt
@@ -2,15 +2,16 @@ fun main() {
     println(findYears(650000.0))
 }
 
-
-
 fun findYears(depo: Double): Int{
     var deposit = depo
     val interestRate = 1.071
     val max = 700000
     var years = 0
     //
-    // implement the while loop here
-    //
+    while(deposit <= max){
+        deposit = deposit*interestRate
+        years++
+        println("année $years -> $deposit $")
+    }
     return years
 }
