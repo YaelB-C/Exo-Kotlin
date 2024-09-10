01f01537e2b61aca4d048daa81ecb8ef43b6645d
YaelB-C <yburand2003@gmail.com>
2024-09-10 08:41:34 +0200

    exo 10sept

--- 15-Structures_de_controle/15_1-if/P15_1_a-Positif_negatif_nul/src/main/kotlin/main.kt
+++ 15-Structures_de_controle/15_1-if/P15_1_a-Positif_negatif_nul/src/main/kotlin/main.kt
@@ -1,3 +1,14 @@
 fun main() {
-    // write your code here
+    val number = -5
+
+    if (number < 0){
+        println("negative")
+    }
+    else if(number > 0){
+        println("negative")
+    }
+    else{
+        println("negative")
+    }
+
 }
