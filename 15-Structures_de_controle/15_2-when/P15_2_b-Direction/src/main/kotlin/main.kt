8c6dc71aba0f32382f4df9d9214420ab6443a3a8
YaelB-C <yburand2003@gmail.com>
2024-09-10 10:30:53 +0200

    exo 10sept

--- 15-Structures_de_controle/15_2-when/P15_2_b-Direction/src/main/kotlin/main.kt
+++ 15-Structures_de_controle/15_2-when/P15_2_b-Direction/src/main/kotlin/main.kt
@@ -1,3 +1,11 @@
 fun main() {
-    // write your code here
+    val direction = readln()
+
+    when(direction ) {
+        "1" -> println("move up")
+        "2" -> println("move down")
+        "3" -> println("move left")
+        "4" -> println("move right")
+        "5" -> println("do not move")
+    }
 }
