ec9781898048aa0bbb2e52d460e08af9e9e6746c
YaelB-C <yburand2003@gmail.com>
2024-09-24 11:31:46 +0200

    exo 24sept

--- 19-POO/19_3-Methodes/P19_3_b-Cube_volume/src/main/kotlin/main.kt
+++ 19-POO/19_3-Methodes/P19_3_b-Cube_volume/src/main/kotlin/main.kt
@@ -1,8 +1,9 @@
 class Box(var height: Double, var width: Double, var length: Double) {
-
-    // write the getVolume method here
+    fun getVolume(): Double {
+        var volume = height * width * length
+        return volume
+    }
 }
-
 
 fun main() {
     val myBox = Box(10.0, 20.0, 50.0)
